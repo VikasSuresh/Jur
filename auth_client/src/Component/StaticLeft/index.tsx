4c817e3c66cc26bdb06c5ba43e0ec83c525bb29a
Vikas <vsvikassvs@gmail.com>
2021-10-05 00:27:21 +0530

    Added Nav Component and StaticLeft

---
+++ auth_client/src/Component/StaticLeft/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+
+const StaticLeft = () => (
+    <div className="col-sm-4" style={{ height: 'vmax', background: '#f4f5fa' }}>
+        <a className="navbar-brand " href="/">
+            <img className="nameImage" src="name.png" alt="Jur" />
+        </a>
+        <h1 className="text">
+            Become a modern
+            {' '}
+            <br />
+            arbitator, Now.
+        </h1>
+        <img className="userImage" src="user.png" alt="User" />
+    </div>
+
+);
+
+export default StaticLeft;
