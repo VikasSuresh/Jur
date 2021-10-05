c8640d2373f7aabeb747e30598fc4978c2be4fce
Vikas <vsvikassvs@gmail.com>
2021-10-05 11:05:57 +0530

    exporting components

---
+++ auth_client/src/Component/index.ts
@@ -0,0 +1,11 @@
+import Nav from './Nav';
+import StaticLeft from './StaticLeft';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+
+export {
+    Nav,
+    StaticLeft,
+    SignIn,
+    SignUp,
+};
