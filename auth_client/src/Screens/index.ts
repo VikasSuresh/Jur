2dfbc0d9f8bf39cb83924ab3aeb82bbcacc1d745
Vikas <vsvikassvs@gmail.com>
2021-10-23 15:22:23 +0530

    changing into reusable component

--- auth_client/src/Screens/index.ts
+++ auth_client/src/Screens/index.ts
@@ -1,9 +1,7 @@
-import Login from './Login';
-import Register from './Register';
+import Auth from './Auth';
 import Home from './Home';
 
 export {
-    Login,
-    Register,
+    Auth,
     Home,
 };
